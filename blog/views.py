12712909c2df9bcf240f587e23d663ee06ebed15
Sangheun Lee <aisiterugh@snu.ac.kr>
2016-06-03 18:41:49 +0900

    blog.post_list 뷰에서 post 목록 노출

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
-    return render(request, 'blog/index.html')
+    post_list = Post.objects.all()
+    return render(request, 'blog/index.html', {
+        'post_list': post_list
+        })
+
