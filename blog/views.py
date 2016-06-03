307ecbeeeb49c0ad28b1aa89521afb0154987c36
Sangheun Lee <aisiterugh@snu.ac.kr>
2016-06-03 20:30:20 +0900

    댓글 작성/수정 시 알림 뜨는 기능 추가

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
@@ -23,6 +24,7 @@ def comment_new(request, post_pk):
             comment.post = get_object_or_404(Post, pk=post_pk)
             comment.user = request.user
             comment.save()
+            messages.success(request, '새로운 댓글이 등록되었습니다.')
             return redirect('blog:post_detail', post_pk)
     else:
         form = CommentForm()
@@ -41,6 +43,7 @@ def comment_edit(request, post_pk, pk):
             comment.post = get_object_or_404(Post, pk=post_pk)
             comment.user = request.user
             comment.save()
+            messages.success(request, '댓글이 수정되었습니다.')
             return redirect('blog:post_detail', post_pk)
     else:
         form = CommentForm(instance=comment)
